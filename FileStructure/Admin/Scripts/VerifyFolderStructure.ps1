2020ed9a342039850275bc28c450c31d5246e3f8
Dave Wyatt <dlwyatt@e-wyatt.org>
2013-10-09 23:28:03 -0400

    Started working on HTML index functionality
    Generated an HTML file based on sample_index.html, but then noticed that
    the sample and the requirements in the PDF document aren't quite the
    same.  Can tweak the contents of the index later, if needed.

--- FileStructure/Admin/Scripts/VerifyFolderStructure.ps1
+++ FileStructure/Admin/Scripts/VerifyFolderStructure.ps1
@@ -20,6 +20,13 @@ function CheckFolder
     # Similar to Compare-Object, outputs PSObjects for differences containing the path to the folder that's different, and a value indicating
     # whether the folder is only on disk or only in the CSV file.
 
+    # Also appends HTML code for the index file into the StringBuilder specified by the $Html parameter, if present.
+
+    # TODO:  The HTML format in the sample index.html and in the pdf requirements document are different.  I drafted this code based on the sample file,
+    # but it will probably need an update shortly to match the more detailed requirements.  It just means changing the current unordered lists at each
+    # level of "subject" folder into <H1>, <H2>, etc, and adding an extra function to enumerate the contents of subject folders (two levels deep) to add
+    # unordered lists.
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -28,7 +35,13 @@ function CheckFolder
 
         [Parameter(Mandatory = $true)]
         [psobject]
-        $Node
+        $Node,
+
+        [System.Text.StringBuilder]
+        $Html = $null,
+
+        [System.UInt32]
+        $Indent = 0
     )
 
     $childFoldersChecked = @{}
@@ -48,10 +61,38 @@ function CheckFolder
         }
         else
         {
+            if ($null -ne $Html)
+            {
+                $uri = New-Object System.Uri($dirInfo.FullName)
+                $code = "<li><a href=""$($uri.AbsoluteUri)"">$($dirInfo.Name)</a>"
+                $null = $Html.Append(("{0,$Indent}{1}" -f ' ', $code))
+            }
+
             # If this is not a "leaf node" of the CSV file (in other words, a subject folder), recursively check its contents
             if ($childNode.Children.Count -gt 0)
             {
-                CheckFolder -Directory $dirInfo -Node $childNode
+                if ($null -ne $Html)
+                {
+                    $null = $Html.AppendLine()
+
+                    $Indent += 2
+                    $null = $Html.AppendLine(("{0,$Indent}<ul>" -f ' '))
+                }
+                
+                CheckFolder -Directory $dirInfo -Node $childNode -Html $Html -Indent ($Indent + 2)
+                
+                if ($null -ne $Html)
+                {
+                    $null = $Html.AppendLine("{0,$Indent}</ul>" -f ' ')
+                    $Indent -= 2
+
+                    $null = $Html.Append(("{0,$Indent}" -f ' '))
+                }
+            }
+
+            if ($null -ne $Html)
+            {
+                $null = $Html.AppendLine("</li>")
             }
         }
     }
@@ -116,8 +157,6 @@ catch
 # The idea is to compare what's in the CSV file with what's actually on disk under the root folder.  If any folders specified in the CSV are missing, or
 # if any extra folders exist on disk, let the user know (through various reporting methods spelled out in the requirements document).
 
-# The trick is to make the script not complain about valid subfolders of subject folders in the CSV file.  Will figure out options for that as the code is written.
-
 # First, convert the CSV file into a hierarchy of objects representing the folders
 
 $rootNode = New-Object psobject -Property @{
@@ -156,6 +195,72 @@ ForEach-Object {
     }
 }
 
-# Now recursively enumerate folders under $dataFolder, looking for differences.
+# Begin forming HTML index code.
 
-CheckFolder -Directory $dataFolder -Node $rootNode
+foreach ($fileName in ('header.inc', 'footer.inc'))
+{
+    $filePath = Join-Path -Path $scriptFolder -ChildPath $fileName
+    if (-not (Test-Path -Path $FilePath))
+    {
+        throw New-Object System.IO.FileNotFoundException($filePath)
+    }
+}
+
+$html = New-Object System.Text.StringBuilder
+
+try
+{
+    $null = $html.AppendLine([System.IO.File]::ReadAllText((Join-Path -Path $scriptFolder -ChildPath 'header.inc')))
+}
+catch
+{
+    throw
+}
+
+$null = $html.AppendLine('<h1>Directory listing</h1>').AppendLine()
+$null = $html.AppendLine('<ul id="dirListing">')
+
+# Now recursively enumerate folders under $dataFolder, looking for differences.  This function also generates HTML code as it goes, appending it to the
+# $html StringBuilder.
+
+$differences = CheckFolder -Directory $dataFolder -Node $rootNode -Html $html
+
+# TODO:  Write various options for reporting the differences.  For now, just output to the screen.
+
+if ($differences.Count -gt 0)
+{
+    Write-Warning "Differences between the disk's folder structure and the '$CsvPath' file were detected:"
+
+    $onlyOnDisk = $differences |
+    Where-Object { $_.DifferenceType -eq [DifferenceType]::OnlyOnDisk } |
+    Select-Object -ExpandProperty Path
+
+    $onlyInCsv = $differences |
+    Where-Object { $_.DifferenceType -eq [DifferenceType]::OnlyInCSV } |
+    Select-Object -ExpandProperty Path
+
+    if ($onlyOnDisk.Count -gt 0)
+    {
+        Write-Warning "Folders that exist on disk, but not in the CSV file:`r`n$($onlyOnDisk | Out-String)"
+    }
+
+    if ($onlyInCsv.Count -gt 0)
+    {
+        Write-Warning "Folders that are defined in the CSV file, but were not found on disk:`r`n$($onlyInCsv | Out-String)"
+    }
+}
+
+# Finish generating HTML and save index file.
+
+$null = $html.AppendLine('</ul>')
+
+try
+{
+    $null = $html.AppendLine([System.IO.File]::ReadAllText((Join-Path -Path $scriptFolder -ChildPath 'footer.inc')))
+}
+catch
+{
+    throw
+}
+
+Set-Content -Path (Join-Path -Path $rootFolder -ChildPath 'Admin\index.html') -Value $html.ToString() -Force
